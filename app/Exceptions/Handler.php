1247cc090be8b226fbeb6e83d30347de5de4fab9
FelixKLG <felix@felixklg.dev>
2022-12-11 18:34:10 +0000

    feat: :goal_net: Add Sentry error reporting
    Sentry now reports error that occur in the program so that they can be reviewed without opening the logs locally.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,7 +44,9 @@ class Handler extends ExceptionHandler
         });
 
         $this->reportable(function (Throwable $e) {
-            //
+            if (app()->bound('sentry')) {
+                app('sentry')->captureException($e);
+            }
         });
     }
 }
