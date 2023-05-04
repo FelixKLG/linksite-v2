6b793752f160475a471b82f24f17309867018efb
FelixKLG <felix@felixklg.dev>
2023-05-04 19:20:35 +0100

    fix: :chart_with_upwards_trend: Fix analytics tracking URL

--- config/analytics.php
+++ config/analytics.php
@@ -4,7 +4,7 @@ return [
     
     'analytics_enabled' => env('ANALYTICS_ENABLED', false),
     
-    'umami_url' => env('UMAMI_URL', 'https://aly.felixklg.dev/VwDrVUy9RFqbFd6.js'),
+    'umami_url' => env('UMAMI_URL', 'https://aly.felixklg.dev/VwDrVUy9RFqbFd6'),
     'umami_id' => env('UMAMI_ID', '628aaba7-ea63-438a-b5c6-da6f5b4d7ad0'),
 
 ];
